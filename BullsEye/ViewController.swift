fa1f07564b87fb701b047ce6919c5fa0909ffeac
arminta7 <Arminta@Sarahs-MacBook-Air-4.local>
2014-11-22 13:04:30 -0800

    Finished Game

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
 	var currentValue: Int = 50
@@ -25,6 +26,25 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		startNewGame()
 		updateLabels()
+		
+		// Customize the slider
+		let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
+		slider.setThumbImage(thumbImageNormal, forState: .Normal)
+		
+		let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")
+		slider.setThumbImage(thumbImageHighlighted, forState: .Highlighted)
+		
+		let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+		
+		let trackLeftImage = UIImage(named: "SliderTrackLeft")
+		let trackLeftResizable = trackLeftImage?.resizableImageWithCapInsets(insets)
+		
+		slider.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
+		
+		let trackRightImage = UIImage(named: "SliderTrackRight")
+		let trackRightResizable = trackRightImage?.resizableImageWithCapInsets(insets)
+		
+		slider.setMaximumTrackImage(trackLeftResizable, forState: .Normal)
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 	
@@ -49,6 +69,12 @@ class ViewController: UIViewController {
 		currentValue = 50
 		startNewRound()
 		updateLabels()
+		
+		let transition = CATransition()
+		transition.type = kCATransitionFade
+		transition.duration = 1
+		transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+		view.layer.addAnimation(transition, forKey: nil)
 	}
 	
 	func calculateScore() {
