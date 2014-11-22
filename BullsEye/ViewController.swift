df6adb7ae3760255ab53d85fc1673b3969e17d04
arminta7 <Arminta@Sarahs-MacBook-Air-4.local>
2014-11-21 19:11:18 -0800

    Bulls Eye Game

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Sarah Anderson on 11/19/14.
+//  Copyright (c) 2014 Sarah Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	var currentValue: Int = 50
+	var targetValue: Int = 0
+	var roundScore = 0
+	var score: Int = 0
+	var round: Int = 0
+	
+	@IBOutlet weak var hitMeButton: UIButton!
+	@IBOutlet weak var slider: UISlider!
+	@IBOutlet weak var targetLabel: UILabel!
+	@IBOutlet weak var roundLabel: UILabel!
+	@IBOutlet weak var scoreLabel: UILabel!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		startNewGame()
+		updateLabels()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	func startNewRound() {
+		targetValue = 1 + Int(arc4random_uniform(100))
+		currentValue = 50
+		slider.value = Float(currentValue)
+		round++
+		score = score + roundScore
+	}
+	
+	func updateLabels() {
+		targetLabel.text = String(targetValue)
+		roundLabel.text = String(round)
+		scoreLabel.text = String(score)
+	}
+	
+	func startNewGame() {
+		round = 0
+		roundScore = 0
+		score = 0
+		currentValue = 50
+		startNewRound()
+		updateLabels()
+	}
+	
+	func calculateScore() {
+		let valueDifference = abs(currentValue - targetValue)
+		
+		roundScore = 100 - valueDifference
+		
+		if valueDifference == 0 {
+			roundScore += 100
+		} else if valueDifference == 1 {
+			roundScore += 50
+		}
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	//Alert popup
+	@IBAction func showAlert() {
+		calculateScore()
+		
+		var title: String
+		if roundScore == 200 {
+			title = "Perfect!"
+		} else if roundScore > 95 {
+			title = "You almost had it!"
+		} else if roundScore > 90 {
+			title = "Pretty good!"
+		} else {
+			title = "Not even close..."
+		}
+		
+		let message = "You scored \(roundScore) points"
+		let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+		let action = UIAlertAction(title: "OK", style: .Default, handler: { action in
+			self.startNewRound()
+			self.updateLabels()
+			})
+		alert.addAction(action)
+		presentViewController(alert, animated: true, completion: nil)
+		
+		//startNewRound()
+		//updateLabels()
+	}
+	
+	@IBAction func sliderMoved(slider: UISlider) {
+		//lroudf rounds the value to the nearest integer
+		currentValue = lroundf(slider.value)
+	}
+	
+	@IBAction func refreshButton() {
+		startNewGame()
+	}
+
+}
+
